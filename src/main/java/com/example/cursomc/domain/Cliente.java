3d019486c4dfb0a3cb9cb0054791d4068129a1d3
Carlos Caique <caique-medeiros2011@hotmail.com>
2022-01-13 22:09:16 -0300

    Costumização : Email nao repetido

--- src/main/java/com/example/cursomc/domain/Cliente.java
+++ src/main/java/com/example/cursomc/domain/Cliente.java
@@ -20,7 +20,10 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+
+	@Column(unique = true)
 	private String email;
+
 	private String cpfOuCnpj;
 	private Integer tipo;
 
